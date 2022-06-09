4bfb194e977c3a16d191077772b371ad8a655596
Vu Ngoc Thinh <vnt462@gmail.com>
2022-06-09 12:16:46 +0700

    update AccountServiceImpl

--- src/main/java/com/fu/flix/service/impl/AccountServiceImpl.java
+++ src/main/java/com/fu/flix/service/impl/AccountServiceImpl.java
@@ -148,7 +148,7 @@ public class AccountServiceImpl implements UserDetailsService, AccountService {
         smsService.sendAndSaveOTP(sms);
 
         CheckUsernameResponse response = new CheckUsernameResponse();
-        response.setMessage("NEW_ACCOUNT_VALID_TEST");
+        response.setMessage(NEW_ACCOUNT_VALID);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 

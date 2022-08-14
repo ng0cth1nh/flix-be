ae1931216e609203f4419e06cc3582f075edf65b
Vu Ngoc Thinh <vnt462@gmail.com>
2022-08-15 00:29:22 +0700

    disable cors

--- src/main/java/com/fu/flix/configuration/SecurityConfig.java
+++ src/main/java/com/fu/flix/configuration/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
+        http.cors().and().csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.authorizeRequests().antMatchers(appConf.getPermitAllApis())
                 .permitAll();
